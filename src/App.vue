c32922a809894a6963bcc1e2e31bfc58dcb08a9d
Gabriel Sansigolo <gabrielsansigolo@gmail.com>
2019-02-22 15:29:03 -0300

    Fix Repo Name

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
 
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <router-link class="navbar-brand" to="/">Knowledge Hub</router-link>
+        <router-link class="navbar-brand" to="/">Open Data Plataform</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
